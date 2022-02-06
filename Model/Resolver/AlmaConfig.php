f9c0f856b313e0d7b9056c7d82cc87c4c61854fb
Francois-Gomis <97046219+Francois-Gomis@users.noreply.github.com>
2022-02-06 23:28:06 +0100

    Init magento 2 GraphQL repo

---
+++ Model/Resolver/AlmaConfig.php
@@ -0,0 +1,57 @@
+<?php
+namespace Alma\GraphQL\Model\Resolver;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Alma\MonthlyPayments\Helpers\Logger;
+use Alma\MonthlyPayments\Gateway\Config\Config;
+
+
+class AlmaConfig implements ResolverInterface
+{
+    /**
+     * @var Logger
+     */
+    private Logger $logger;
+    /**
+     * @var Config
+     */
+    private Config $almaConfig;
+
+    public function __construct(
+        Logger $logger,
+        Config $almaConfig
+    ) {
+        $this->logger = $logger;
+        $this->almaConfig = $almaConfig;
+    }
+
+    /**
+     * @param Field $field
+     * @param \Magento\Framework\GraphQl\Query\Resolver\ContextInterface $context
+     * @param ResolveInfo $info
+     * @param array|null $value
+     * @param array|null $args
+     * @return array|\Magento\Framework\GraphQl\Query\Resolver\Value|mixed
+     * @throws GraphQlInputException
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        $almaConfig = $value['model'];
+        return [
+            'model'=> $almaConfig,
+            'is_enable' => $this->almaConfig->getIsActive(),
+            'mode' => $this->almaConfig->getActiveMode(),
+            'title' => $this->almaConfig->getPaymentButtonTitle(),
+            'description' => $this->almaConfig->getPaymentButtonDescription(),
+            'sort_order' => $this->almaConfig->getSortOrder(),
+        ];
+    }
+}
---
+++ Model/Resolver/AlmaFeePlans.php
@@ -0,0 +1,82 @@
+<?php
+namespace Alma\GraphQL\Model\Resolver;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
+use Alma\MonthlyPayments\Helpers\Logger;
+use Alma\MonthlyPayments\Helpers\Eligibility;
+
+class AlmaFeePlans implements ResolverInterface
+{
+    public function __construct(
+        Logger $logger,
+        Eligibility $eligibility
+    ) {
+        $this->logger = $logger;
+        $this->eligibility = $eligibility;
+    }
+
+    /**
+     * @param Field $field
+     * @param \Magento\Framework\GraphQl\Query\Resolver\ContextInterface $context
+     * @param ResolveInfo $info
+     * @param array|null $value
+     * @param array|null $args
+     * @return array|\Magento\Framework\GraphQl\Query\Resolver\Value|mixed
+     * @throws GraphQlInputException
+     */
+    public function resolve(
+        Field $field,
+              $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        $almaEligibility = $value['model'];
+        $paymentPlans = [];
+        $feePlans = $this->getFeePlans();
+        foreach ($feePlans as $key=> $plan){
+            $planConfig      = $plan->getPlanConfig()->toArray();
+            $planEligibility = $plan->getEligibility();
+            $paymentPlans[$key]['key'] = $planConfig['key'];
+            $paymentPlans[$key]['logo'] = $planConfig['logo'];
+            $paymentPlans[$key]['allowed'] = $planConfig['allowed'];
+            $paymentPlans[$key]['kind'] = $planConfig['kind'];
+            $paymentPlans[$key]['installments_count'] = $planConfig['installmentsCount'];
+            $paymentPlans[$key]['deferred_days'] = $planConfig['deferredDays'];
+            $paymentPlans[$key]['deferred_months'] = $planConfig['deferredMonths'];
+            $paymentPlans[$key]['enabled'] = $planConfig['enabled'];
+            $paymentPlans[$key]['min_amount'] = $planConfig['minAmount'];
+            $paymentPlans[$key]['max_amount'] = $planConfig['maxAmount'];
+            $paymentPlans[$key]['customer_lending_rate'] = $planConfig['customerLendingRate'];
+            $paymentPlans[$key]['deferred_type'] = $planConfig['deferredType'];
+            $paymentPlans[$key]['deferred_duration'] = $planConfig['deferredDuration'];
+            $paymentPlans[$key]['eligibility']['is_eligible'] = $planEligibility->isEligible;
+            $paymentPlans[$key]['eligibility']['reasons'] = $planEligibility->reasons;
+            $paymentPlans[$key]['eligibility']['constraints'] = $planEligibility->constraints;
+            foreach ($planEligibility->paymentPlan as $feePlan){
+                $paymentPlans[$key]['eligibility']['fee_plans'][] =$feePlan;
+            }
+        }
+        return array_values($paymentPlans);
+    }
+
+    /**
+     * Get fee plans
+     *
+     * @return array
+     */
+    private function getFeePlans(): array
+    {
+        $this->logger->info('get Alma Fee plans in Graph QL resolver',[]);
+        $plans = $this->eligibility->getCurrentsFeePlans();
+        if (!$this->eligibility->isAlreadyLoaded()){
+            $plans=$this->eligibility->getEligiblePlans();
+        }
+        return $plans;
+    }
+}
