cc66cf38db3185c7b5e9157fb3097a5868f99c55
Francois Gomis <francois.gomis@getalma.eu>
2022-02-09 15:37:21 +0100

    feat: setPaymentMethodAndPlaceOrder

---
+++ Model/AlmaPlanDataProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Alma\GraphQL\Model;
+
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\QuoteGraphQl\Model\Cart\Payment\AdditionalDataProviderInterface;
+use Alma\MonthlyPayments\Helpers\Logger;
+
+/**
+ * Format Alma input into value expected when setting payment method
+ */
+class AlmaPlanDataProvider implements AdditionalDataProviderInterface
+{
+    public function __construct(
+        Logger $logger
+    ) {
+        $this->logger = $logger;
+    }
+    private const PATH_ADDITIONAL_DATA = 'alma';
+
+    /**
+     * Format Alma input into value expected when setting payment method
+     *
+     * @param array $args
+     * @return array
+     * @throws GraphQlInputException
+     */
+    public function getData(array $args): array
+    {
+        $this->logger->info('AdditionalDataProviderInterface args',[$args]);
+        if (!isset($args[self::PATH_ADDITIONAL_DATA])) {
+            throw new GraphQlInputException(
+                __('Required parameter "alma" for "payment_method" is missing.')
+            );
+        }
+        return $args[self::PATH_ADDITIONAL_DATA];
+    }
+}
